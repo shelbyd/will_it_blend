9c9b82b29a9a60c9543a13a3403cf7802c4cf463
Shelby Doolittle <shelby@shelbyd.com>
2016-12-20 17:46:35 -0500

    add really simple copyable text

--- main.lua
+++ main.lua
@@ -1,3 +1,28 @@
-function HelloWorld()
-  print("Hello, World!");
+WillItBlend = LibStub("AceAddon-3.0"):NewAddon("WillItBlend")
+AceConsole = LibStub("AceConsole-3.0")
+
+function WillItBlend:OnInitialize()
+  AceConsole:Print("Hello, World!")
+  AceConsole:RegisterChatCommand("blend", HandleBlendChatCommand)
+end
+
+function WillItBlend:OnEnable()
+end
+
+function WillItBlend:OnDisable()
+end
+
+function HandleBlendChatCommand(input)
+  -- local CharacterProfile = GetCharacterProfile()
+  -- local SimulationCraftProfile = SimulationCraftProfile(CharacterProfile)
+  -- ShowCopyableText(SimulationCraftProfile:ToString())
+  ShowCopyableText("This is copyable!")
+end
+
+function ShowCopyableText(text)
+  WibCopyFrame:Show()
+  WibCopyFrameScroll:Show()
+  WibCopyFrameScrollText:Show()
+  WibCopyFrameScrollText:SetText(text)
+  WibCopyFrameScrollText:HighlightText()
 end
